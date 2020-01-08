287305b798b4058b584453c75debbaa6bf848e10
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-01-08 20:23:36 +0700

    added new model

--- application/models/KKModel.php
+++ application/models/KKModel.php
@@ -6,4 +6,41 @@ class KKModel extends CI_Model
         $query = $this->db->get('kartukeluarga');
         return $query->num_rows();
     }
+
+    public function getAllKK() {
+        $query = $this->db->get('kartukeluarga');
+        return $query->result_array();
+    }
+
+    public function addDataKK($nokk, $nik, $status, $ayah, $ibu) {
+        $data = array(
+            'no_kk' => $nokk,
+            'nik' => $nik,
+            'status_dalam_keluarga' => $status,
+            'ayah' => $ayah,
+            'ibu' => $ibu
+        );
+
+        $query = $this->db->insert('kartukeluarga', $data);
+        return $query;
+    }
+
+    public function editDataKK($id, $nokk, $nik, $status, $ayah, $ibu) {
+        $this->db->set('no_kk', $nokk);
+        $this->db->set('nik', $nik);
+        $this->db->set('status_dalam_keluarga', $status);
+        $this->db->set('ayah', $ayah);
+        $this->db->set('ibu', $ibu);
+        $this->db->where('id', $id);
+
+        $query = $this->db->update('kartukeluarga');
+        return $query;
+
+    }
+
+    public function deleteDataKK($id) {
+        $data = array('id' => $id);
+        $query = $this->db->delete('kartukeluarga', $data);
+        return $query;
+    }
 }
--- application/models/PendudukModel.php
+++ application/models/PendudukModel.php
@@ -6,4 +6,46 @@ class PendudukModel extends CI_Model
         $query = $this->db->get('penduduk');
         return $query->result_array();
     }
+
+    public function getSpecificationPenduduk($nik) {
+        $query = $this->db->get_where('penduduk', array('nik' => $nik));
+        return $query->result_array();
+    }
+
+    public function editDataPenduduk($nik, $nama, $tempat, $tgl, $jk, $rtrw, $pekerjaan, $kewarganegaraan, $pendidikanterakhir, $agama, $golongandarah) {
+        $this->db->set('nama', $nama);
+        $this->db->set('tempat', $tempat);
+        $this->db->set('tgl_lahir', $tgl);
+        $this->db->set('JK', $jk);
+        $this->db->set('RT/RW', $rtrw);
+        $this->db->set('pekerjaan', $pekerjaan);
+        $this->db->set('kewarganegaraan', $kewarganegaraan);
+        $this->db->set('pendidikan_terakhir', $pendidikanterakhir);
+        $this->db->set('agama', $agama);
+        $this->db->set('gol_darah', $golongandarah);
+        $this->db->where('nik', $nik);
+        
+        $query = $this->db->update('penduduk');
+
+        return $query;
+    }
+
+    public function saveDataPenduduk($nik, $nama, $tempat, $tgl, $jk, $rtrw, $pekerjaan, $kewarganegaraan, $pendidikanterakhir, $agama, $golongandarah) {
+        $data = array(
+            'nik' => $nik,
+            'nama' => $nama,
+            'tempat' => $tempat,
+            'tgl_lahir' => $tgl,
+            'JK' => $jk,
+            'RT/RW' => $rtrw,
+            'pekerjaan' => $pekerjaan,
+            'kewarganegaraan' => $kewarganegaraan,
+            'pendidikan_terakhir' => $pendidikanterakhir,
+            'agama' => $agama,
+            'gol_darah' => $golongandarah 
+        );
+
+        $query = $this->db->insert('penduduk', $data);
+        return $query;
+    }
 }
